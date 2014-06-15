14b0ce6c887ca4df5b2a483c70a285aa64a8fa20
José Ladislao Lainez Ortega <thedragonladis@gmail.com>
2014-06-15 18:01:00 +0200

    Add abstract class to represent any renderizable asset.

---
+++ Engine/Assets/RenderizableAsset.hpp
@@ -0,0 +1,19 @@
+#ifndef OPENGE_RENDERIZABLEASSET_H
+#define OPENGE_RENDERIZABLEASSET_H
+
+#include "Engine/Assets/Asset.hpp"
+#include "Engine/Core/Math/Vector3.hpp"
+#include "Engine/Core/Math/Quaternion.hpp"
+
+class RenderizableAsset : public Asset
+{
+protected:
+	RenderizableAsset(const std::string name, const std::string filePath) : Asset(name, filePath) { }
+	virtual ~RenderizableAsset() { }
+
+public:
+	virtual void			Render const (const Vector3& pos, const Quaternion& rot, const Vector3& scale) = 0;
+
+};
+
+#endif // OPENGE_RENDERIZABLEASSET_H
