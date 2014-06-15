fd8973b7d773921afbd69205285fada35707d0f7
José Ladislao Lainez Ortega <thedragonladis@gmail.com>
2014-06-15 17:59:57 +0200

    Add abstract class Asset to represent game's assets.

--- Engine/Assets/Asset.hpp
+++ Engine/Assets/Asset.hpp
@@ -4,10 +4,12 @@
 #include <string>
 #include "Engine/Libraries/Libraries.hpp"
 
+class ResourceManager;
+
 class Asset
 {
 protected:
-	Asset(const std::string name) : name(name) { };
+	Asset(const std::string name, const std::string filePath) : name(name), filePath(filePath) { };
 	virtual ~Asset();
 
 	inline Uint16 const Usage() { return usage; }
@@ -15,10 +17,13 @@ protected:
 	inline void			NotifyRelease() { if (usage > 0) --usage; }
 
 public:
-	std::name			name;
+	std::string			name;
+	std::string			filePath;
 
 protected:
 	Uint16				usage;
+
+	friend class ResourceManager;
 };
 
 #endif // OPENGE_ASSET_H
