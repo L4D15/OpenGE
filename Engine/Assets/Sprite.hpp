e68bbe9d6849fba5bd9146a0415188260b9fe0b3
José Ladislao Lainez Ortega <thedragonladis@gmail.com>
2014-06-17 10:08:55 +0200

    Add multi-frame animations to Sprite class.

--- Engine/Assets/Sprite.hpp
+++ Engine/Assets/Sprite.hpp
@@ -2,21 +2,81 @@
 #define OPENGE_SPRITE_H
 
 #include "Engine/Assets/RenderizableAsset.hpp"
+#include "Engine/Libraries/Libraries.hpp"
+#include <map>
 
 class ResourceManager;
+class TestScene;
 
 class Sprite : public RenderizableAsset
 {
-protected:
-	Sprite(const std::string name, const std::string filePath);
-	virtual ~Sprite();
+public:
+    // Animation Class
+    class Animation
+    {
+    public:
+        Animation(const float x, const float y,
+                  const float w, const float h,
+                  const unsigned int frames,
+                  const float frameRate,
+                  const std::string name) :
+                x(x), y(y),
+                w(w), h(h),
+                frames(frames), frameRate(frameRate),
+                name(name)
+        {
 
-	virtual void 				Render(const Vector3& pos, const Quaternion& rot, const Vector3& scale) const;
+        }
+
+        const SDL_Rect ToSDLRect(unsigned int frame) const
+        {
+            SDL_Rect rect;
+
+            rect.x = x + ((w * frame) % frames);
+            rect.y = y;
+            rect.w = w;
+            rect.h = h;
+
+            return rect;
+        }
+
+    public:
+        int x, y;
+        unsigned int w, h, frames;
+        float frameRate;
+        std::string name;
+    };
+    // ---
+
+protected:
+    Sprite(const std::string name, const std::string filePath);
+    virtual ~Sprite();
+
+    virtual void                Render(const Vector3& pos, const Quaternion& rot, const Vector3& scale) const;
+    virtual void                Render(const std::string animation,
+                                       const unsigned int frame,
+                                       const Vector3& pos,
+                                       const Quaternion& rot,
+                                       const Vector3& scale) const;
+
+    void                        AddAnimation(const std::string name,
+                                             const int x, const int y,
+                                             const int w, const int h,
+                                             const int frames,
+                                             const float frameRate);
 
 public:
-	virtual std::string			ToString() const;
+    virtual std::string         ToString() const;
 
-	friend class ResourceManager;
+protected:
+    SDL_Texture*                texture;
+    int                         width;
+    int                         height;
+    std::map<std::string,
+                 Animation>     animationsMapper;
+
+    friend class ResourceManager;
+    friend class TestScene;
 };
 
 
