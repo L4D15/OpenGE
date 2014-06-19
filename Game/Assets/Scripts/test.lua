7598dbc2c4a8a075ba1f77c2b2448417e5c49d8d
José Ladislao Lainez Ortega <thedragonladis@gmail.com>
2014-06-20 01:52:44 +0200

    Improve example code for animation.

--- Game/Assets/Scripts/test.lua
+++ Game/Assets/Scripts/test.lua
@@ -4,15 +4,30 @@ Test["Update"] = function()
 	transform = self:GetComponent_Transform()
 	renderer = self:GetComponent_SpriteRenderer()
 
-    pressedButton = input:GetButton("Walk Right")
-    --print(Game.GetTime().GetDeltaTime())
+    if (input:GetButton("Walk Right") == true) then
 
-    if (pressedButton == true) then
     	renderer:ChangeAnimation("Right")
         movement = Vector3(100.0, 0.0, 0.0) * Time.GetDeltaTime()
         transform.position = transform.position + movement
-    else
+
+    elseif (input:GetButton("Walk Left") == true) then
+
+    	renderer:ChangeAnimation("Left")
+    	movement = Vector3(-100.0, 0.0, 0.0) * Time.GetDeltaTime()
+    	transform.position = transform.position + movement
+
+    elseif (input:GetButton("Walk Up")) then
+
+    	renderer:ChangeAnimation("Back")
+    	movement = Vector3(0.0, -100.0, 0.0) * Time.GetDeltaTime()
+    	transform.position = transform.position + movement
+
+    elseif (input:GetButton("Walk Down")) then
+
     	renderer:ChangeAnimation("Front")
+    	movement = Vector3(0.0, 100.0, 0.0) * Time.GetDeltaTime()
+    	transform.position = transform.position + movement
+
     end
 end
 
