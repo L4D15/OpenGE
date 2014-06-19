8632a1b4a6db336d3245645e79e060de508c5abf
José Ladislao Lainez Ortega <thedragonladis@gmail.com>
2014-06-20 01:42:37 +0200

    Fix wrong template image's animation names.

--- Game/Assets/Scripts/test.lua
+++ Game/Assets/Scripts/test.lua
@@ -2,14 +2,17 @@ Test = {}
 
 Test["Update"] = function()
 	transform = self:GetComponent_Transform()
+	renderer = self:GetComponent_SpriteRenderer()
 
     pressedButton = input:GetButton("Walk Right")
     --print(Game.GetTime().GetDeltaTime())
 
     if (pressedButton == true) then
+    	renderer:ChangeAnimation("Right")
         movement = Vector3(100.0, 0.0, 0.0) * Time.GetDeltaTime()
         transform.position = transform.position + movement
-        print(transform.position:ToString())
+    else
+    	renderer:ChangeAnimation("Front")
     end
 end
 
