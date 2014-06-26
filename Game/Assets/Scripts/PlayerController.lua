3e94cea899f9ea627d77944a6f99a1a9636bc3bd
José Ladislao Lainez Ortega <thedragonladis@gmail.com>
2014-06-26 22:16:59 +0200

    Fix bug related to different objects with same script.

--- Game/Assets/Scripts/PlayerController.lua
+++ Game/Assets/Scripts/PlayerController.lua
@@ -1,32 +1,31 @@
 function PlayerController(init)
     local self = {
         transform,
-        sprite,
-        number
+        sprite
     }
 
     function self.Start()
         print(string.format("Starting %s", gameObject.name))
-        transform = gameObject:GetTransform()
-        sprite = gameObject:GetSpriteRenderer()
+        self.transform = gameObject:GetTransform()
+        self.sprite = gameObject:GetSpriteRenderer()
     end
 
     function self.Update()
         if (input:GetButton("Walk Left")) then
-            transform.position = transform.position + Vector3(-100.0, 0.0, 0.0) * Time.GetDeltaTime()
-            sprite:ChangeAnimation("Left")
+            self.transform.position = self.transform.position + Vector3(-100.0, 0.0, 0.0) * Time.GetDeltaTime()
+            self.sprite:ChangeAnimation("Left")
 
         elseif (input:GetButton("Walk Right")) then
-            transform.position = transform.position + Vector3(100.0, 0.0, 0.0) * Time.GetDeltaTime()
-            sprite:ChangeAnimation("Right")
+            self.transform.position = self.transform.position + Vector3(100.0, 0.0, 0.0) * Time.GetDeltaTime()
+            self.sprite:ChangeAnimation("Right")
 
         elseif (input:GetButton("Walk Up")) then
-            transform.position = transform.position + Vector3(0.0, -100.0, 0.0) * Time.GetDeltaTime()
-            sprite:ChangeAnimation("Back")
+            self.transform.position = self.transform.position + Vector3(0.0, -100.0, 0.0) * Time.GetDeltaTime()
+            self.sprite:ChangeAnimation("Back")
 
         elseif (input:GetButton("Walk Down")) then
-            transform.position = transform.position + Vector3(0.0, 100.0, 0.0) * Time.GetDeltaTime()
-            sprite:ChangeAnimation("Front")
+            self.transform.position = self.transform.position + Vector3(0.0, 100.0, 0.0) * Time.GetDeltaTime()
+            self.sprite:ChangeAnimation("Front")
 
         end
     end
